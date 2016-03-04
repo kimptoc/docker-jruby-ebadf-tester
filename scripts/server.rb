908dd29a7950f486a10736f10d43de2b822acfc9
Chris Kimpton <chris@kimptoc.net>
2016-03-04 12:39:39 +0000

    use nested threads

--- scripts/server.rb
+++ scripts/server.rb
@@ -3,13 +3,16 @@
 require_relative 'common'
 
 Thread.new do
-  begin
-    sleep 1
-    require_relative 'includes/include1'
-    require_relative 'includes/include2'
-    puts "MY_INCLUDE=#{MY_INCLUDE}"
-  rescue Exception => ex
-    puts "#{ex.message}/#{ex.class}"
+  sleep 1
+  Thread.new do
+    begin
+      sleep 1
+      require_relative 'includes/include1'
+      require_relative 'includes/include2'
+      puts "MY_INCLUDE=#{MY_INCLUDE}"
+    rescue Exception => ex
+      puts "#{ex.message}/#{ex.class}"
+    end
   end
 end
 
